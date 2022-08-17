07c0c45acf58a3870a7efd76272e4a640c8c921d
YousafUsman <110670387+YousafUsman@users.noreply.github.com>
2022-08-17 15:50:41 +0500

    Updates

---
+++ precache-manifest.45cc092b5da2cdc503bad7d1ef8303ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbbfc828d167dbbc712",
+    "url": "/YousafUsman.github.io/test/static/css/main.50d4be05.chunk.css"
+  },
+  {
+    "revision": "efbbfc828d167dbbc712",
+    "url": "/YousafUsman.github.io/test/static/js/main.efbbfc82.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/YousafUsman.github.io/test/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "d2a60a3e8d8297467d00",
+    "url": "/YousafUsman.github.io/test/static/js/2.d2a60a3e.chunk.js"
+  },
+  {
+    "revision": "3b18b46f795ec52b894f",
+    "url": "/YousafUsman.github.io/test/static/js/runtime~main.3b18b46f.js"
+  },
+  {
+    "revision": "5a0b18356c5237b9163e9f768dfeac78",
+    "url": "/YousafUsman.github.io/test/index.html"
+  }
+];
